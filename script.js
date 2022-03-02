7f63ad3ce30329ee13298673fa31e3086de15b61
Nischal Gurung <nizallgurung16@gmail.com>
2022-03-02 13:58:37 +0530

    Finish project

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
+const progress = document.querySelector("#progress");
+const prev = document.querySelector("#prev");
+const next = document.querySelector("#next");
+const steps = document.querySelectorAll(".steps");
 
+let currentActive = 1;
+next.addEventListener("click", function () {
+  currentActive++;
+  if (currentActive > steps.length) {
+    currentActive = steps.length;
+  }
+  update();
+});
+prev.addEventListener("click", function () {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  update();
+});
+function update() {
+  steps.forEach((step, index) => {
+    if (index < currentActive) {
+      step.classList.add("active");
+    } else {
+      step.classList.remove("active");
+    }
+  });
+  const actives = document.querySelectorAll(".active");
+  progress.style.width =
+    ((actives.length - 1) / (steps.length - 1)) * 100 + "%";
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === steps.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+}
